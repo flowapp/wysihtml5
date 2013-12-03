68e6674f2c9a12e22f042afe34c184ee63678ad5
Bjarne Mogstad <bjarne@metalabdesign.com>
2013-12-03 16:05:57 +0100

    Sharpen list breaking logic

--- src/wysihtml5/keyboard/break_list.js
+++ src/wysihtml5/keyboard/break_list.js
@@ -6,20 +6,20 @@ var LIST_TAGS = ["UL", "OL", "MENU"];
 
 Composer.RegisterKeyboardHandler(function(e) {
   return (
-    e.type === "keydown" && 
+    e.type === "keydown" &&
     e.keyCode == Constants.ENTER_KEY
   );
 }, function(editor, composer, e) {
-  var blockElement = dom.getParentElement(composer.selection.getSelectedNode(), { 
-    nodeName: ["LI"] 
+  var listItem = dom.getParentElement(composer.selection.getSelectedNode(), {
+    nodeName: ["LI"]
   }, 4);
 
-  if (blockElement) {
+  if (listItem) {
     setTimeout(function() {
       var selectedNode = composer.selection.getSelectedNode();
-      var list = dom.getParentElement(selectedNode, { 
+      var list = dom.getParentElement(selectedNode, {
         nodeName: LIST_TAGS
-      }, 2);
+      });
 
       if (!list) {
         composer.selection.executeAndRestore(function() {
