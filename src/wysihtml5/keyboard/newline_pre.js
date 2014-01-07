364204b7baaaa5c32b9f63ae488fef97cbc18c48
Bjarne Mogstad <bjarne@metalabdesign.com>
2014-01-07 16:45:00 +0100

    Implement pre breaking

--- src/wysihtml5/keyboard/newline_pre.js
+++ src/wysihtml5/keyboard/newline_pre.js
@@ -2,22 +2,41 @@ import { Constants } from "../constants";
 import { Composer } from "../views/composer";
 import { browser } from "../browser";
 import dom from "../dom";
+import { insertParagraphAfter } from "../helpers/insert_paragraph_after";
 
 Composer.RegisterKeyboardHandler(function(e) {
   return (
     e.type === "keydown" &&
-    e.keyCode == Constants.ENTER_KEY
+    e.keyCode == Constants.ENTER_KEY &&
+    !e.ctrlKey
   );
 }, function(editor, composer, e) {
-  var selectedNode = composer.selection.getSelectedNode();
-  var preElement = dom.getParentElement(selectedNode, {
+  var preElement = dom.getParentElement(composer.selection.getSelectedNode(), {
     nodeName: ["PRE"]
   });
 
-  var range = composer.selection.getRange().nativeRange;
-
   if (preElement) {
+    preElement.normalize();
+    var selectedNode = composer.selection.getSelectedNode();
+    var range = composer.selection.getRange().nativeRange;
     e.preventDefault();
+
+    var newLineRange = document.createRange();
+    newLineRange.selectNodeContents(selectedNode);
+
+    var atEndOfNode = composer.selection.caretIsAtEndOfNode(newLineRange, preElement);
+    if (range.collapsed && atEndOfNode) {
+      if (range.startContainer.nodeType === Node.TEXT_NODE) {
+        var content = range.startContainer.textContent.substr(Math.max(range.startOffset - 2, 0), 2);
+        if (content == "\n\n") {
+          insertParagraphAfter(preElement, composer);
+          var text = range.startContainer.textContent;
+          range.startContainer.textContent = text.slice(0, text.length - 2);
+          return;
+        }
+      }
+    }
+
     if (!range.collapsed) {
       range.deleteContents();
     }
