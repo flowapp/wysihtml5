fc3ae97c31773582e2cd4d0f5681f980df7feef6
Bjarne Mogstad <bjarne@metalabdesign.com>
2014-01-07 21:50:15 +0100

    Rework pre-formatted text logic

--- src/wysihtml5/commands/formatBlock.js
+++ src/wysihtml5/commands/formatBlock.js
@@ -1,7 +1,7 @@
 import dom from "../dom";
 import lang from "wysihtml5/lang";
-import { browser } from "../browser";
 import { Constants } from "../constants";
+import { fromPlainText } from "../helpers/from_plain_text";
 
 var BLOCK_ELEMENTS_GROUP = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "PRE", "BLOCKQUOTE", "DIV"];
 
@@ -27,85 +27,15 @@ function _removeClass(element, classRegExp) {
   return ret;
 }
 
-/**
- * Check whether given node is a text node and whether it's empty
- */
-function _isBlankTextNode(node) {
-  return node.nodeType === Node.TEXT_NODE && !lang.string(node.data).trim();
-}
-
-/**
- * Returns previous sibling node that is not a blank text node
- */
-function _getPreviousSiblingThatIsNotBlank(node) {
-  var previousSibling = node.previousSibling;
-  while (previousSibling && _isBlankTextNode(previousSibling)) {
-    previousSibling = previousSibling.previousSibling;
-  }
-  return previousSibling;
-}
-
-/**
- * Returns next sibling node that is not a blank text node
- */
-function _getNextSiblingThatIsNotBlank(node) {
-  var nextSibling = node.nextSibling;
-  while (nextSibling && _isBlankTextNode(nextSibling)) {
-    nextSibling = nextSibling.nextSibling;
-  }
-  return nextSibling;
-}
-
-/**
- * Adds line breaks before and after the given node if the previous and next siblings
- * aren't already causing a visual line break (block element or <br>)
- */
-function _addLineBreakBeforeAndAfter(node) {
-  var doc             = node.ownerDocument,
-      nextSibling     = _getNextSiblingThatIsNotBlank(node),
-      previousSibling = _getPreviousSiblingThatIsNotBlank(node);
-
-  if (nextSibling && !_isLineBreakOrBlockElement(nextSibling)) {
-    node.parentNode.insertBefore(doc.createElement("br"), nextSibling);
-  }
-  if (previousSibling && !_isLineBreakOrBlockElement(previousSibling)) {
-    node.parentNode.insertBefore(doc.createElement("br"), node);
-  }
-}
-
-function _isLineBreak(node) {
-  return node.nodeName === "BR";
-}
-
-/**
- * Checks whether the elment causes a visual line break
- * (<br> or block elements)
- */
-function _isLineBreakOrBlockElement(element) {
-  if (_isLineBreak(element)) {
-    return true;
-  }
-
-  if (dom.getStyle("display").from(element) === "block") {
-    return true;
-  }
-
-  return false;
-}
-
-function _hasClasses(element) {
-  return !!lang.string(element.className).trim();
-}
-
 var formatBlock = {
   exec: function(composer, command, nodeName, className, classRegExp) {
-    var doc             = composer.doc,
-        blockElements    = this.state(composer, command, nodeName, className, classRegExp),
-        useLineBreaks   = composer.config.useLineBreaks,
-        defaultNodeName = useLineBreaks ? "DIV" : "P",
-        selectedNodes, classRemoveAction, blockRenameFound;
+    var blockElements = this.state(composer, command, nodeName, className, classRegExp);
+    var defaultNodeName = "P";
+    var classRemoveAction;
 
     nodeName = typeof(nodeName) === "string" ? nodeName.toUpperCase() : nodeName;
+    nodeName = nodeName || "P";
+
     if (blockElements.length) {
       composer.selection.executeAndRestoreSimple(function() {
         for (var b = blockElements.length; b--;) {
@@ -117,51 +47,13 @@ var formatBlock = {
             // dont rename or remove element when just setting block formating class
             return;
           }
-
-          var hasClasses = _hasClasses(blockElements[b]);
-
-          if (!hasClasses && (useLineBreaks || nodeName === "P")) {
-            // Insert a line break afterwards and beforewards when there are siblings
-            // that are not of type line break or block element
-            _addLineBreakBeforeAndAfter(blockElements[b]);
-            dom.replaceWithChildNodes(blockElements[b]);
-          } else if (nodeName === "PRE") {
-            var element = blockElements[b];
-            var content = element.innerHTML;
-            var paragraphs = dom.fromPlainText(content, true);
-            var fragment = dom.nodeList.toFragment(paragraphs);
-            element.parentNode.replaceChild(fragment, element);
-          } else {
-            // Make sure that styling is kept by renaming the element to a <div> or <p> and copying over the class name
-            dom.renameElement(blockElements[b], nodeName === "P" ? "DIV" : defaultNodeName);
-          }
+          dom.renameElement(blockElements[b], nodeName === "P" ? "DIV" : defaultNodeName);
         }
       });
-
-      return;
-    }
-
-    if (nodeName === null || lang.array(BLOCK_ELEMENTS_GROUP).contains(nodeName)) {
-      if (nodeName == "PRE") {
-        var node = composer.selection.surround({nodeName: "PRE"});
-        if (node) {
-          var children = dom.all.elements(node);
-          children.reverse().forEach(function(child) {
-            if (child.nodeName == "BR") {
-              child.parentNode.replaceChild(document.createTextNode("\n"), child);
-            } else {
-              if (Constants.BLOCK_ELEMENTS.indexOf(child.nodeName) != -1 && node.lastChild != child) {
-                child.innerHTML += "\n\n";
-              }
-              dom.replaceWithChildNodes(child);
-            }
-          });
-        }
-        return;
-      }
-
+    } else if (nodeName === null || lang.array(BLOCK_ELEMENTS_GROUP).contains(nodeName)) {
       // Find similiar block element and rename it (<h2 class="foo"></h2>  =>  <h1 class="foo"></h1>)
-      selectedNodes = composer.selection.findNodesInSelection(BLOCK_ELEMENTS_GROUP).concat(composer.selection.getSelectedOwnNodes());
+      var selectedNodes = composer.selection.findNodesInSelection(BLOCK_ELEMENTS_GROUP);
+      selectedNodes = selectedNodes.concat(composer.selection.getSelectedOwnNodes());
 
       composer.selection.executeAndRestoreSimple(function() {
         for (var n = selectedNodes.length; n--;) {
@@ -186,15 +78,13 @@ var formatBlock = {
   },
 
   state: function(composer, command, nodeName, className, classRegExp) {
-    var nodes = composer.selection.getSelectedOwnNodes(),
-        parents = [],
-        parent;
+    var nodes = composer.selection.getSelectedOwnNodes();
+    var parents = [];
 
     nodeName = typeof(nodeName) === "string" ? nodeName.toUpperCase() : nodeName;
 
-    //var selectedNode = composer.selection.getSelectedNode();
     for (var i = 0, maxi = nodes.length; i < maxi; i++) {
-      parent = dom.getParentElement(nodes[i], {
+      var parent = dom.getParentElement(nodes[i], {
         nodeName: nodeName,
         className: className,
         classRegExp: classRegExp
