8c3c18c8fd3236a31e1315199d103f1c75d2b798
Rose <rose@metalabdesign.com>
2014-01-08 16:41:34 -0800

    Cache previousSibling

--- src/wysihtml5/dom/remove_trailing_line_breaks.js
+++ src/wysihtml5/dom/remove_trailing_line_breaks.js
@@ -9,9 +9,11 @@ import { nodeList } from "./node_list";
 
 var removeTrailingLineBreaks = function(node) {
   var childNodes = nodeList.toArray(node.querySelectorAll("br:last-child"));
+
   for (var index = 0; index < childNodes.length; index++) {
     var childNode = childNodes[index];
-    if(!childNode.previousSibling || (childNode.previousSibling && childNode.previousSibling.nodeName != "BR")) {
+    var previousSibling = childNode.previousSibling;
+    if(!previousSibling || (previousSibling && previousSibling.nodeName != "BR")) {
       childNode.parentNode.removeChild(childNode);
     }
   }
