48a5e49e273b97a5273162fd7f117bb91f97590d
Bjarne Mogstad <bjarne@metalabdesign.com>
2014-04-21 01:57:28 +0200

    Add support for setting a collapsed range in “SetupContentEditable”

--- test/integration_tests/lib/setup_content_editable.js
+++ test/integration_tests/lib/setup_content_editable.js
@@ -1,6 +1,7 @@
 var wd = require("wd");
 
 function SetupContentEditable(content, url) {
+  content = content || "";
   url = url || "http://localhost:8000/test/integration_tests/html/index.html";
   var item;
   return browser.get(url).then(function() {
@@ -11,7 +12,7 @@ function SetupContentEditable(content, url) {
   }).then(function() {
     return browser.execute("arguments[0].innerHTML = arguments[1];", [item, content]);
   }).then(function() {
-    if (content.indexOf("[") === -1) {
+    if (content.indexOf("[") === -1 && content.indexOf("|") === -1) {
       return item.type("a"+ wd.SPECIAL_KEYS["Back space"]);
     } else {
       return browser.execute(SetSelection.toString() +"; SetSelection(arguments[0]);", [item]);
@@ -35,6 +36,13 @@ function SetSelection(parent) {
     index = text.indexOf("]");
     if (index !== -1) {
       node.nodeValue = text = text.replace("]", "");
+      range.setEnd(node, index);
+    }
+
+    index = text.indexOf("|");
+    if (index !== -1) {
+      node.nodeValue = text = text.replace("|", "");
+      range.setStart(node, index);
       range.setEnd(node, index);
     }
   });
