6943fc0c088926126be71b8717071d46fb4be91a
Bjarne Mogstad <bjarne@metalabdesign.com>
2014-04-21 01:47:03 +0200

    Setup support for coffeescript in tests

--- tasks/options/mochaSelenium.js
+++ tasks/options/mochaSelenium.js
@@ -3,8 +3,15 @@ module.exports = {
     options: {
       reporter: "spec",
       timeout: 30e3,
-      usePromises: true
+      usePromises: true,
+      coffeeScript: true,
+      browsers: [
+        {browserName: "firefox"}
+      ]
     },
-    src: ['test/integration_tests/**/*.test.js'],
+    src: [
+      'test/integration_tests/**/*.test.coffee',
+      'test/integration_tests/**/*.test.js'
+    ],
   }
 };
