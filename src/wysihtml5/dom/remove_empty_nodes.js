752a156dcde9c613e22e416e3cabee52f0a4ca5e
Rose <rosemarie.pritchard@gmail.com>
2014-06-05 12:26:30 -0700

    Switch to a for loop

--- src/wysihtml5/dom/remove_empty_nodes.js
+++ src/wysihtml5/dom/remove_empty_nodes.js
@@ -6,25 +6,21 @@
  *    wysihtml5.dom.removeEmptyNodes(element);
  */
 
+import { nodeList } from "../dom/node_list";
+
 var removeEmptyNodes = function(node) {
-  var index, lastNodeIndex, element;
-  element = node.cloneNode(true);
-  index = 0;
-  while (index != element.childNodes.length) {
-    var nodeContainsImage, nodeHasTextContent;
-    var node = element.childNodes[index];
+  var element = node.cloneNode(true);
+  var nodes = nodeList.toArray(element.childNodes);
+  for (var index = 0; index < nodes.length; index++) {
+    var node = nodes[index];
     if (node) {
-      nodeContainsImage = node.nodeName == "IMG" || (node.nodeType == 1 && node.querySelector("img"));
-      nodeHasTextContent = (node.textContent || "").trim();
+      var nodeContainsImage = node.nodeName == "IMG" || (node.nodeType == Node.ELEMENT_NODE && node.querySelector("img"));
+      var nodeHasTextContent = (node.textContent || "").trim();
+      if (!nodeContainsImage && !nodeHasTextContent) {
+        element.removeChild(node);
+      }
     }
-
-    if (!nodeContainsImage && !nodeHasTextContent) {
-      element.removeChild(node);
-    } else {
-      index++;
-    }
-  }
-
+  };
   return element;
 };
 
