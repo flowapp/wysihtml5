4e73f10a7793d70f7ff36d586f16c5425b37de2e
Bjarne Mogstad <bjarne@metalabdesign.com>
2015-02-03 22:34:51 +0100

    Update dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -1,39 +1,37 @@
-module.exports = function (broccoli) {
-  var staticCompiler = require('broccoli-static-compiler');
-  var mergeTrees = require('broccoli-merge-trees');
-  var replace = require('broccoli-replace');
-  var compileModules = require('broccoli-es6-module-transpiler');
+var staticCompiler = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+var replace = require('broccoli-replace');
+var compileModules = require('broccoli-es6-module-transpiler');
 
-  var packageJSON = require('./package.json');
+var packageJSON = require('./package.json');
 
-  var vendorTree = staticCompiler('vendor', {
-    srcDir: '/',
-    destDir: 'vendor'
-  });
+var vendorTree = staticCompiler('vendor', {
+  srcDir: '/',
+  destDir: 'vendor'
+});
 
-  var application = new mergeTrees([vendorTree, 'src'], {overwrite: true})
+var application = new mergeTrees([vendorTree, 'src'], {overwrite: true})
 
-  application = replace(application, {
-    files: [
-      '**/*.js'
-    ],
-    patterns: [
-      {
-        match: 'version',
-        replacement: packageJSON.version
-      },
-      {
-        match: 'repo',
-        replacement: packageJSON.repository.url
-      }
-    ]
-  });
+application = replace(application, {
+  files: [
+    '**/*.js'
+  ],
+  patterns: [
+    {
+      match: 'version',
+      replacement: packageJSON.version
+    },
+    {
+      match: 'repo',
+      replacement: packageJSON.repository.url
+    }
+  ]
+});
 
-  application = compileModules(application, {
-    format: 'bundle',
-    entry: 'wysihtml5.js',
-    output: 'wysihtml5.js'
-  });
+application = compileModules(application, {
+  format: 'bundle',
+  entry: 'wysihtml5.js',
+  output: 'wysihtml5.js'
+});
 
-  return application;
-};
+module.exports = application;
