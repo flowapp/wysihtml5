ef6f36e616f597b135b0ac78a2b748955b1eb004
Rose Robertson <rose@r.osey.me>
2017-05-10 14:23:16 -0700

    Update nonprintable keys to include the cmd key

--- src/wysihtml5/helpers/is_non_printable_key.js
+++ src/wysihtml5/helpers/is_non_printable_key.js
@@ -1,6 +1,6 @@
 var isNonPrintableKey = function(keyCode) {
   var nonPrintableKeys, modifierKeys, arrowKeys, fKeys;
-  modifierKeys = [8, 9, 13, 16, 17, 18, 19, 20, 27, 33, 34, 35, 26, 46, 93];
+  modifierKeys = [8, 9, 13, 16, 17, 18, 19, 20, 27, 33, 34, 35, 26, 46, 93, 224];
   arrowKeys = [37, 38, 39, 40, 91, 92];
   fKeys = [112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 144, 145];
 
